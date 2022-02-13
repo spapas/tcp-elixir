205eb6827d1e2d9063c785299c590d43c4687582
Serafeim Papastefanos <spapas@gmail.com>
2022-02-13 22:19:53 +0200

    Register handlers

--- lib/tcp/connection_handler.ex
+++ lib/tcp/connection_handler.ex
@@ -3,6 +3,7 @@ defmodule Tcp.ConnectionHandler do
 
   @impl ThousandIsland.Handler
   def handle_connection(socket, state) do
+    Tcp.Registry.register(self(), socket)
     ThousandIsland.Socket.send(socket, "Hello, World")
     IO.puts("Connected!")
     {:continue, state}
@@ -12,6 +13,12 @@ defmodule Tcp.ConnectionHandler do
   def handle_data(msg, _socket, state) do
     IO.puts(msg)
     {:continue, state}
+  end
+
+  @impl ThousandIsland.Handler
+  def handle_close(_socket, _state) do
+    IO.puts("Disconnected!")
+    Tcp.Registry.unregister(self())
   end
 
   def handle_info({:send, msg}, {socket, state}) do
